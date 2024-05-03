b7218b3725a6ab18ea3bc12d1a9ed5c4355c46b8
Steve Vissault <s.vissault@yahoo.fr>
2024-05-03 15:46:04 -0400

    Adjust DDL and inject field sample table

--- sql/db_create_ddl.sql
+++ sql/db_create_ddl.sql
@@ -4,6 +4,10 @@ DROP TABLE IF EXISTS species;
 
 DROP TABLE IF EXISTS sites;
 
+DROP TABLE IF EXISTS project;
+
+DROP TABLE IF EXISTS report;
+
 DROP TABLE IF EXISTS field_sample;
 
 DROP TABLE IF EXISTS lab_sample;
@@ -11,8 +15,6 @@ DROP TABLE IF EXISTS lab_sample;
 DROP TABLE IF EXISTS lab_measurement;
 
 DROP TABLE IF EXISTS analyte;
-
-DROP TABLE IF EXISTS project;
 
 CREATE TABLE species -- Create a reference table for each species involved in study
 (
@@ -26,64 +28,12 @@ CREATE TABLE species -- Create a reference table for each species involved in st
 
 CREATE TABLE sites -- Create reference table for each site location
 (
-    id_name_site TEXT PRIMARY KEY,
+    id_site TEXT PRIMARY KEY,
     name_en TEXT,
     province TEXT NOT NULL,
     lat FLOAT,
     lon FLOAT,
     srid INTEGER
-);
-
-CREATE TABLE field_sample -- Create a new table which document collected field samples
-(
-    id_field_sample TEXT PRIMARY KEY,
-    id_site TEXT NOT NULL,
-    id_species TEXT NOT NULL,
-    id_project TEXT,
-    age TEXT,
-    tissue TEXT,
-    collection_date TEXT,
-    FOREIGN KEY(id_site) REFERENCES sites(id_site),
-    FOREIGN KEY(id_species) REFERENCES species(id_species),
-    FOREIGN KEY(id_project) REFERENCES project(id_project)
-);
-
-CREATE TABLE lab_sample -- Create a new table which document all lab sample
--- Lab sample could be one or multiple field sample pooled
-(
-    id_lab_sample TEXT PRIMARY KEY,
-    id_field_sample TEXT NOT NULL,
-    id_project TEXT,
-    analyze_date TEXT NOT NULL,
-    id_source_report TEXT,
-    path_source_report TEXT,
-    note TEXT,
-    FOREIGN KEY(id_field_sample) REFERENCES field_sample(id_field_sample),
-    FOREIGN KEY(id_project) REFERENCES project(id_project)
-);
-
-CREATE TABLE lab_measurement -- Create a new table which contains lab measurements
-(
-    id_lab_measurement INTEGER PRIMARY KEY,
-    id_analyte TEXT NOT NULL,
-    measure FLOAT NOT NULL,
-    is_censored INTEGER NOT NULL DEFAULT 0,
-    id_lab_sample TEXT NOT NULL,
-    performed_on_dry_weight INTEGER NOT NULL DEFAULT 1,
-    performed_on_isolated_lipid INTEGER NOT NULL DEFAULT 0,
-    note TEXT,
-    FOREIGN KEY(id_lab_sample) REFERENCES lab_sample(id_lab_sample),
-    FOREIGN KEY(id_analyte) REFERENCES analyte(id_analyte)
-);
-
-CREATE TABLE analyte -- Create table which contains analyte description provided by the lab
-(
-    id_analyte TEXT PRIMARY KEY,
-    unit TEXT NOT NULL,
-    family TEXT NOT NULL,
-    cas_number TEXT,
-    alias TEXT,
-    note TEXT
 );
 
 CREATE TABLE project -- Create table which contains project metadata description in association with field and/or lab samples
@@ -96,4 +46,62 @@ CREATE TABLE project -- Create table which contains project metadata description
     email_investigator TEXT NOT NULL,
     email_data_manager TEXT NOT NULL,
     description TEXT
+);
+
+CREATE TABLE report -- Create table which contains project metadata description in association with field and/or lab samples
+(
+    id_report TEXT NOT NULL PRIMARY KEY,
+    id_project TEXT,
+    report_date TEXT,
+    report_access_path TEXT,
+    FOREIGN KEY(id_project) REFERENCES project(id_project)
+);
+
+CREATE TABLE analyte -- Create table which contains analyte description provided by the lab
+(
+    id_analyte TEXT PRIMARY KEY,
+    unit TEXT NOT NULL,
+    family TEXT NOT NULL,
+    cas_number TEXT,
+    alias TEXT,
+    note TEXT
+);
+
+CREATE TABLE field_sample -- Create a new table which document collected field samples
+(
+    id_field_sample TEXT PRIMARY KEY,
+    id_site TEXT NOT NULL,
+    id_species TEXT NOT NULL,
+    id_report TEXT NOT NULL,
+    age TEXT,
+    tissue TEXT,
+    collection_date TEXT,
+    FOREIGN KEY(id_site) REFERENCES sites(id_site),
+    FOREIGN KEY(id_species) REFERENCES species(id_species),
+    FOREIGN KEY(id_report) REFERENCES report(id_report)
+);
+
+CREATE TABLE lab_sample -- Create a new table which document all lab sample
+-- Lab sample could be one or multiple field sample pooled
+(
+    id_lab_sample TEXT PRIMARY KEY,
+    id_field_sample TEXT NOT NULL,
+    id_project TEXT,
+    id_source_report TEXT,
+    path_source_report TEXT,
+    note TEXT,
+    FOREIGN KEY(id_field_sample) REFERENCES field_sample(id_field_sample),
+    FOREIGN KEY(id_project) REFERENCES project(id_project)
+);
+
+CREATE TABLE lab_measurement -- Create a new table which contains lab measurements
+(
+    id_lab_measurement INTEGER PRIMARY KEY,
+    id_analyte TEXT NOT NULL,
+    id_lab_sample TEXT NOT NULL,
+    measure FLOAT NOT NULL,
+    is_censored INTEGER NOT NULL DEFAULT 0,
+    note TEXT,
+    FOREIGN KEY(id_lab_sample) REFERENCES lab_sample(id_lab_sample),
+    FOREIGN KEY(id_analyte) REFERENCES analyte(id_analyte)
 );
