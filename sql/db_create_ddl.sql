9378d71605fd0fb508c493b3eec34004683c904e
Steve Vissault <s.vissault@yahoo.fr>
2024-05-29 18:56:37 -0400

    Update database DDL

--- sql/db_create_ddl.sql
+++ sql/db_create_ddl.sql
@@ -1,10 +1,10 @@
 PRAGMA foreign_keys = ON;
 
-DROP TABLE IF EXISTS field_sample;
-
 DROP TABLE IF EXISTS lab_sample;
 
 DROP TABLE IF EXISTS lab_measurement;
+
+DROP TABLE IF EXISTS field_sample;
 
 DROP TABLE IF EXISTS species;
 
@@ -15,6 +15,11 @@ DROP TABLE IF EXISTS report;
 DROP TABLE IF EXISTS project;
 
 DROP TABLE IF EXISTS analyte;
+
+DROP TABLE IF EXISTS itgr_measurement_source;
+
+DROP TABLE IF EXISTS itgr_analyte_source;
+
 
 CREATE TABLE species -- Create a reference table for each species involved in study
 (
@@ -36,6 +41,18 @@ CREATE TABLE sites -- Create reference table for each site location
     srid INTEGER
 );
 
+CREATE TABLE field_sample -- Create a new table which document collected field samples
+(
+    id_field_sample TEXT NOT NULL PRIMARY KEY,
+    collection_date TEXT,
+    id_site TEXT NOT NULL,
+    id_species TEXT NOT NULL,
+    age TEXT,
+    tissue TEXT,
+    FOREIGN KEY(id_site) REFERENCES sites(id_site) ON UPDATE CASCADE,
+    FOREIGN KEY(id_species) REFERENCES species(id_species) ON UPDATE CASCADE
+);
+
 CREATE TABLE project -- Create table which contains project metadata description in association with field and/or lab samples
 (
     id_project TEXT PRIMARY KEY,
@@ -54,51 +71,65 @@ CREATE TABLE report -- Create table which contains project metadata description 
     id_project TEXT,
     report_date TEXT,
     report_access_path TEXT,
-    FOREIGN KEY(id_project) REFERENCES project(id_project)
-);
-
-CREATE TABLE analyte -- Create table which contains analyte description provided by the lab
-(
-    id_analyte TEXT PRIMARY KEY,
-    unit TEXT NOT NULL,
-    family TEXT NOT NULL,
-    cas_number TEXT,
-    alias TEXT,
-    note TEXT
-);
-
-CREATE TABLE field_sample -- Create a new table which document collected field samples
-(
-    id_field_sample TEXT NOT NULL PRIMARY KEY,
-    collection_date TEXT,
-    id_site TEXT NOT NULL,
-    id_species TEXT NOT NULL,
-    age TEXT,
-    tissue TEXT,
-    FOREIGN KEY(id_site) REFERENCES sites(id_site),
-    FOREIGN KEY(id_species) REFERENCES species(id_species)
+    FOREIGN KEY(id_project) REFERENCES project(id_project) ON UPDATE CASCADE
 );
 
 CREATE TABLE lab_sample -- Create a new table which document all lab sample
 -- Lab sample could be one or multiple field sample pooled
 (
     id_lab_sample TEXT NOT NULL,
-    id_field_sample TEXT NOT NULL,
+    id_field_sample TEXT,
     id_report TEXT,
     note TEXT,
     PRIMARY KEY (id_lab_sample, id_field_sample, id_report),
-    FOREIGN KEY(id_field_sample) REFERENCES field_sample(id_field_sample),
-    FOREIGN KEY(id_report) REFERENCES report(id_report)
+    FOREIGN KEY(id_field_sample) REFERENCES field_sample(id_field_sample) ON UPDATE CASCADE,
+    FOREIGN KEY(id_report) REFERENCES report(id_report) ON UPDATE CASCADE
+);
+
+CREATE TABLE analyte -- Create table which contains analyte description provided by the lab
+(
+    id_analyte TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    other_name TEXT,
+    short_name TEXT, 
+    unit TEXT,
+    family TEXT,
+    casid TEXT,
+    pubcid INTEGER,
+    note TEXT,
+    is_dry_weight BOOLEAN CHECK (is_dry_weight IN (0, 1)),
+    isotope_on_amino_acid BOOLEAN CHECK (isotope_on_amino_acid IN (0, 1)),
+    on_isolated_lipid BOOLEAN CHECK (on_isolated_lipid IN (0, 1))
+);
+
+CREATE TABLE itgr_analyte_source -- Create a new table which contains lab measurements
+(
+    id_analyte INTEGER PRIMARY KEY,
+    source_file TEXT NOT NULL,
+    note TEXT,
+    FOREIGN KEY(id_analyte) REFERENCES analyte(id_analyte)
 );
 
 CREATE TABLE lab_measurement -- Create a new table which contains lab measurements
 (
-    id_lab_measurement INTEGER PRIMARY KEY,
+    id_lab_sample TEXT NOT NULL,
+    id_analyte TEXT NOT NULL,
+    value FLOAT NOT NULL,
+    is_censored BOOLEAN CHECK (is_censored IN (0, 1)) DEFAULT 0,
+    percent_lipid FLOAT,
+    percent_moisture FLOAT,
+    note TEXT,
+    PRIMARY KEY (id_lab_sample, id_analyte),
+    FOREIGN KEY(id_lab_sample) REFERENCES lab_sample(id_lab_sample) ON UPDATE CASCADE,
+    FOREIGN KEY(id_analyte) REFERENCES analyte(id_analyte) ON UPDATE CASCADE
+);
+
+CREATE TABLE itgr_measurement_source -- Create a new table which contains lab measurements
+(
     id_analyte TEXT NOT NULL,
     id_lab_sample TEXT NOT NULL,
-    measure FLOAT NOT NULL,
-    is_censored INTEGER NOT NULL DEFAULT 0,
+    source_file TEXT NOT NULL,
     note TEXT,
-    FOREIGN KEY(id_lab_sample) REFERENCES lab_sample(id_lab_sample),
-    FOREIGN KEY(id_analyte) REFERENCES analyte(id_analyte)
+    PRIMARY KEY (id_lab_sample, id_analyte),
+    FOREIGN KEY(id_lab_sample, id_analyte) REFERENCES lab_measurement(id_lab_sample, id_analyte) ON UPDATE CASCADE
 );
